f61cd8871fa0c137e18fde6aa881a21b720e4a66
fredmanre <fredmanre@gmail.com>
2019-03-31 09:54:56 -0500

    static

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # para archivos estáticos
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 MEDIA_URL = '/media/'  # carpeta donde iran los media(la ruta)
